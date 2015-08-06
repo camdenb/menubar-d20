28ae9be88c05ae129a6e4e4f0ca9fb0eacbfc63e
camdenb <doctorshmoogle@gmail.com>
2015-08-06 13:50:51 -0700

    Initial Commit

---
+++ Menubar D20/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Menubar D20
+//
+//  Created by Camden Bickel on 8/6/15.
+//  Copyright © 2015 Camden Bickel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
